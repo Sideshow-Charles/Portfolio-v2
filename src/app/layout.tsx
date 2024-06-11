7e144681a7fa0147672e15aa3aae8347a360193d
Sideshow-Charles <charlesonuoha97@gmail.com>
2024-06-11 20:04:42 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-      /* eslint-disable */
       <body className={inter.className}>{children}</body>
     </html>
   );
