b3be85022332c7659ff514b86fe8e5482ea97163
Sideshow-Charles <charlesonuoha97@gmail.com>
2024-06-14 13:56:48 +0100

    tweaked the responsiveness for the about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,12 +25,12 @@ const Home = () => {
         {/* End of CTA Button */}
       </section>
 
-      <section className="flex justify-center items-center flex-col text-center h-96 mt-48">
+      <section className="flex justify-center items-center flex-col text-center h-96 my-48">
         <p className="font-extrabold text-xl text-zinc-700 mb-8 max-md:text-lg">
           Brief Overview
         </p>
         <p className="font-bold mb-8 text-6xl max-md:text-3xl">Sleep, Code && Repeat</p>
-        <p className="text-center w-3/4 text-xl leading-relaxed">
+        <p className="text-center w-3/4 text-xl leading-relaxed max-md:w-full">
           With a keen eye for design and a deep understanding of the latest web
           technologies, I'm a driving force in crafting seamless and intuitive
           interfaces while implementing and maintaining functionality. My
