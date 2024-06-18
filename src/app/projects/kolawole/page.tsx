d928b5ab941e29a6d44e836a260d3a0df5832660
Sideshow-Charles <charlesonuoha97@gmail.com>
2024-06-18 18:37:20 +0100

    responsiveness for the kolaowole project screen

--- src/app/projects/kolawole/page.tsx
+++ src/app/projects/kolawole/page.tsx
@@ -12,11 +12,11 @@ const Kolawole = () => {
             <Header btnclassName="bg-slate-950 text-white py-2 px-4 border border-slate-950 rounded-md mx-2"></Header>
             {/* End of Header Component*/}
 
-            <div className="mx-24">
+            <div className="mx-24 max-md:mx-12 max-sm:mx-6">
 
                 <div className="text-center">
-                    <p className="font-bold text-slate-950 text-5xl mt-24">Kolawole's Portfolio</p>
-                    <p className="mt-8 text-xl leading-loose">Overview, Projects, Work History <br></br>
+                <p className="font-bold text-slate-950 text-5xl mt-24 max-md:text-4xl max-sm:text-3l">Kolawole's Portfolio</p>
+                <p className="mt-8 text-xl leading-loose max-md:text-lg max-sm:text-base">Overview, Projects, Work History <br></br>
                         Of Kolawole, A Digital Product Designer</p>
                     <div className="flex justify-center mt-8">
                         <Link target="_blank" href="https://kola-beige.vercel.app"><Button content="View Live" className="bg-slate-950 text-white py-2 px-4 border border-slate-950 rounded-md mx-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-dark duration-200"></Button></Link>
@@ -32,25 +32,25 @@ const Kolawole = () => {
 
                 <div className="mb-48">
 
-                    <p className="font-bold text-slate-950 text-3xl">Project Description:</p>
-                    <p className="mt-8 text-xl leading-loose">Online Portfolio for Kolawole, A digital product designer.<br></br>
+                <p className="font-bold text-slate-950 text-3xl max-md:text-2xl">Project Description:</p>
+                <p className="mt-8 text-xl leading-relaxed max-md:text-lg leading-loose mt-4 max-sm:text-base">Online Portfolio for Kolawole, A digital product designer.<br></br>
                         Idea was to display projects, work history,  Overview of Kolawole. It was built with the idea of getting to
                         know Kolawole, without meeting him in person.
                     </p>
 
-                    <p className="font-bold text-slate-950 text-3xl mt-24">Tools:</p>
-                    <p className="mt-8 text-xl leading-loose">Nuxt.js (a Vue.js framework), CSS, Javascript(ES6). It was designed by <Link target="_blank" className="underline" href="https://www.linkedin.com/in/kolawole-popoola-b61664143/">Kolawole Popoola.</Link></p>
+                    <p className="font-bold text-slate-950 text-3xl mt-24 max-md:text-2xl">Tools:</p>
+                    <p className="mt-8 text-xl leading-relaxed max-md:text-lg leading-loose mt-4 max-sm:text-base">Nuxt.js (a Vue.js framework), CSS, Javascript(ES6). It was designed by <Link target="_blank" className="underline" href="https://www.linkedin.com/in/kolawole-popoola-b61664143/">Kolawole Popoola.</Link></p>
                 </div>
 
                 <div className="flex justify-between items-center">
                     <div className="text-left">
-                        <p className=" text-xl mt-24 text-zinc-700">Previous</p>
-                        <Link href="/projects/rendezvous"><p className="font-black mt-2 mb-16 text-3xl text-slate-950">RENDEZVOUS</p></Link>
+                    <p className="text-xl mt-24 text-zinc-700 max-md:text-lg max-sm:text-base">Previous</p>
+                        <Link href="/projects/rendezvous"><p className="font-black mt-2 mb-16 text-3xl text-slate-950 max-md:text-2xl">RENDEZVOUS</p></Link>
                     </div>
 
                     <div className="text-right">
-                        <p className=" text-xl mt-24 text-zinc-700">Next</p>
-                        <Link href="/projects/rick-and-morty"><p className="font-black mt-2 mb-16 text-3xl text-slate-950">RICK AND MORTY</p></Link>
+                    <p className="text-xl mt-24 text-zinc-700 max-md:text-lg max-sm:text-base">Next</p>
+                        <Link href="/projects/rick-and-morty"><p className="font-black mt-2 mb-16 text-3xl text-slate-950 max-md:text-2xl">RICK AND MORTY</p></Link>
                     </div>
                 </div>
             </div>
