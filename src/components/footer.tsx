f47177a7eb63c68e65d3f23ae1b3ded10b738918
Sideshow-Charles <charlesonuoha97@gmail.com>
2024-06-18 17:40:53 +0100

    responsiveness in the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="flex justify-between flex-col bg-black text-slate-400 px-24 py-24 text-stone-300 max-md:px-12 max-sm:px-6">
-            <p className="leading-loose mb-16">Built and shipped with love, Shoutout to Spotify for the jams and Apple Podcasts <br></br> for keeping me company through the process!</p>
+            <p className="leading-loose mb-16 text-xl max-md:text-lg max-sm:text-base">Built and shipped with love, Shoutout to Spotify for the jams and Apple Podcasts <br></br> for keeping me company through the process!</p>
             <p>&copy; Charles Onuoha, {(new Date().getFullYear())} All rights reserved.</p>
 
         </div>
